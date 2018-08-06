67d1256ed249307a309454ddd1a5505ab6fc847e
SanjaySharma <sanjay.gemini@gmail.com>
2018-08-06 10:13:19 +0100

    Updating the Content

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 9000);
+app.listen(process.env.PORT || 8080);
